549ac8d994d515ab5433070ae3ed26b9f71fdd97
Ashwani Saraswat <146135006+AshwaniSaraswat99@users.noreply.github.com>
2025-09-14 21:54:00 +0000

    added some more features

--- weatherdetector/weatherdetector/weather/views.py
+++ weatherdetector/weatherdetector/weather/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect 
-from django.contrib import admin
-
+from django.contrib import admin, messages
+import json
+import requests
+from decouple import config
+Weather_API = config('Weather_API')
 # Create your views here.
 def index(request): 
+    weather=None
+    city = ''
     if request.method == "POST":
         city = request.POST['city']
+        response=requests.get(f'https://api.weatherapi.com/v1/current.json?key={Weather_API}&q={city}&aqi=no').json()
+        if "error" in response:
+            error_message = "Invalid city! Please enter a valid city name."
+        weather={
+            'city':city,
+            "country": response["location"]["country"],
+            "time": response["location"]["localtime"],
+            "temp_c": response["current"]["temp_c"],
+            "condition": response["current"]["condition"]["text"],
+            "icon": response["current"]["condition"]["icon"],
+            "wind_kph": response["current"]["wind_kph"],
+            "humidity": response["current"]["humidity"],
+        }
     else:
         city = ''
-    return render(request, 'index.html',{'city':city})
+    return render(request, 'index.html',{'weather':weather})
