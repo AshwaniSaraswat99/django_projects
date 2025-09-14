4f29d236d0db9e46450addbc90edc18f7b64df47
Ashwani Saraswat <146135006+AshwaniSaraswat99@users.noreply.github.com>
2025-09-14 21:33:30 +0000

    it is functional will fetch and  show real time weather data

--- weatherdetector/weatherdetector/weather/views.py
+++ weatherdetector/weatherdetector/weather/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
+from django.contrib import admin
 
 # Create your views here.
-def index(request):
-    return redirect(request, 'index.html')
+def index(request): 
+    if request.method == "POST":
+        city = request.POST['city']
+    else:
+        city = ''
+    return render(request, 'index.html',{'city':city})
